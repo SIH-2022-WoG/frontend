af3d18b73ac0add8f1ef36db5fd8dd1b27e4f85d
Nitin <nk17oct@gmail.com>
2022-08-12 15:06:29 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <Router>
+        <Routes>
+          <Route exact path="/" /*element={<Homepage />}*/ />
+          <Route path="/dashboard" /*element={<Dashboard />}*/ />
+          <Route exact path="/search" /*element={<Search />}*/ />
+          <Route exact path="/theses" /*element={<Theses />}*/ />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
