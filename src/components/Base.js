759812279cc48801a784c0847921488f53e7f251
Nitin <nk17oct@gmail.com>
2022-08-12 20:16:22 +0530

    Navbar

--- src/components/Base.js
+++ src/components/Base.js
@@ -4,7 +4,7 @@ import Navigationbar from "./Navigationbar";
 
 const Base = ({ children, title = "Base" }) => {
   return (
-    <div className="d-flex flex-column min-vh-100 container-fluid bg-dark">
+    <div className="d-flex flex-column min-vh-100 container-fluid bg-dark p-0">
       <Navigationbar />
       <h1 className="text-success m-5">{title}</h1>
       {children}
