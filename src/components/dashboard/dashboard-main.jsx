6c7f2a92081e21fb69669d9a380852027942522c
vedikaag78 <vedika1510@gmail.com>
2022-08-25 11:36:38 +0530

    dashboard cards changed

--- src/components/dashboard/dashboard-main.jsx
+++ src/components/dashboard/dashboard-main.jsx
@@ -119,21 +119,21 @@ export default function Dashboard() {
       <Container maxWidth={false}>
         <Grid container spacing={3}>
           <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <InfoCard title="card 1" value="val 1" avatarbgcolor="error.main"/>
+            <InfoCard title="No. of thesis" value="val 1" avatarbgcolor="error.main"/>
           </Grid>
           <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <InfoCard title="card 1" value="val 1" avatarbgcolor="success.light"/>
+            <InfoCard title="Thesis approved" value="val 2" avatarbgcolor="success.light"/>
           </Grid>
           <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <InfoCard title="card 1" value="val 1" avatarbgcolor="primary.main"/>
+            <InfoCard title="No. of co authors" value="val 3" avatarbgcolor="primary.main"/>
           </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
+          {/* <Grid item xl={3} lg={3} sm={6} xs={12}>
             <InfoCard title="card 1" value="val 1" avatarbgcolor="warning.light"/>
-          </Grid>
+          </Grid> */}
           <Grid item lg={8} md={12} xl={9} xs={12}>
             <Submissions data={data} title="Recent Submissions"/>
           </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
+          {/* <Grid item lg={4} md={6} xl={3} xs={12}>
             <InfoCard title="card 1" value="val 1" />
           </Grid>
           <Grid item lg={4} md={6} xl={3} xs={12}>
@@ -141,7 +141,7 @@ export default function Dashboard() {
           </Grid>
           <Grid item lg={8} md={12} xl={9} xs={12}>
             <Crd />
-          </Grid>
+          </Grid> */}
         </Grid>
       </Container>
     </Box>
