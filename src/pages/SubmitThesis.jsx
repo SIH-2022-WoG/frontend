2785a94761354fc94d2e7c2298ae4854cfe26183
Nitin <nk17oct@gmail.com>
2022-08-25 11:37:32 +0530

    submit thesis file

---
+++ src/pages/SubmitThesis.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Typography } from "@mui/material";
+
+export default function SubmitThesis() {
+  <Container
+    sx={{
+      py: 8,
+    }}
+  >
+    <Typography sx={{ mb: 3 }} variant="h4">
+      Submit
+    </Typography>
+  </Container>;
+}
