84e84fc1a01334c875316a238129986a9613e9be
vedikaag78 <vedika1510@gmail.com>
2022-08-26 06:17:04 +0530

    footer css fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { Grid, Box} from "@mui/material";
 const Footer = () => {
   return (
-    <footer className="mt-auto bg-success">
-      <div className="footer-copyright text-center py-3">
+    <Grid sx={{mt:4}}>
+       <Box display="flex" justifyContent="flex-end" sx={{mr:3}}>
         © 2020 Copyright: <h4>WrathOfGod - SIH 2022</h4>
-      </div>
-    </footer>
+       </Box>
+    </Grid>
   );
 };
 
