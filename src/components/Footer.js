9b9ade7f0c58ee2e439bfe3d9e141e0ba91173ce
vedikaag78 <vedika1510@gmail.com>
2022-08-26 06:19:08 +0530

    ui changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Grid, Box} from "@mui/material";
 const Footer = () => {
   return (
     <Grid sx={{mt:4}}>
-       <Box display="flex" justifyContent="flex-end" sx={{mr:3}}>
+       <Box display="flex" justifyContent="flex-end" sx={{mr:3, mb: 2}}>
         © 2020 Copyright: <h4>WrathOfGod - SIH 2022</h4>
        </Box>
     </Grid>
